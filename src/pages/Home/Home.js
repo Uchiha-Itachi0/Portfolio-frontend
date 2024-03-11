7f5580de5fbf5263a760338689722190d212b092
Anubhav Shukla <anubhav008shukla@gmail.com>
2024-03-11 09:26:04 +0530

    Updated home image

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import HomeStyle from './HomeStyle'
-import MyImg from "../../assets/images/myImg2.jpeg"
+import MyImg from "../../assets/images/port1.jpg"
 import CircularRotatingImg from '../../components/CircularRotatingImg/CircularRotatingImg'
 import styled from 'styled-components'
 import TwoCircleButton from '../../components/TwoCircleButton/TwoCircleButton'
